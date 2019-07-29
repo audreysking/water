c917c9aa1097636a8eceed27af4cf903291657b8
Jin Ruixin <audreysking@gmail.com>
2019-07-29 20:32:23 +0900

    update

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module Water
       g.helper false
       g.test_framework false
     end
-    config.i18n.default_locale = :ja
+    # config.i18n.default_locale = :ja
     config.time_zone = 'Asia/Tokyo'
   end
 end
